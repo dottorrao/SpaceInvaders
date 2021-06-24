53b3c1ab670f89b3ba34a97f6b41fb77da8989e7
Marco <marco.ruggeri@gmail.com>
2021-06-24 15:51:15 +0200

    Issue #
    Make enemy shooting

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -11,9 +11,18 @@ import random
 #============= Inizialization
 #speed of the player
 playerspeed = 10
+
 enemyspeed = 0.05
-bulletspeed = 5
+bulletspeed = 2
+enemy_bullet_speed = 1
 refreshAlienImage = 120
+number_of_enemies = 30
+
+#enemies can shot at maximum 3 bullet at time.
+#this list is update at real time and take note of which bullets are fired and "live"
+#into screem
+#enemy_fires = [0,0,0]
+
 #Define bullet state
 #ready - ready to fire
 #fire - bulllet is firing
@@ -28,6 +37,7 @@ wn.register_shape("crab2.gif")
 wn.register_shape("cannon.gif")
 wn.register_shape("bullet.gif")
 wn.register_shape("explosion.gif")
+wn.register_shape("enemy_laser.gif")
 wn.tracer(0)
 
 #============= Draw border
@@ -65,9 +75,7 @@ player.speed(0)
 player.setposition(0, -250)
 player.setheading(90)
 
-
 #chose a number of enemies
-number_of_enemies = 30
 #create an empty list of enemies
 enemies = []
 
@@ -82,6 +90,7 @@ enemy_number = 0
 for enemy in enemies:
 	#enemy
 	#enemy.color("red")
+	setattr(enemy, "fired", False)
 	enemy.shape("crab1.gif")
 	enemy.penup()
 	enemy.speed(0)
@@ -104,6 +113,36 @@ bullet.speed(0)
 bullet.setheading(90)
 #bullet.shapesize(0.5,0.5)
 bullet.hideturtle()
+
+#we have at maximum three bullets for enemy displayed into screen.
+# it's needed to define all three bullets differently in order to manage them. 
+#enemy fire bullet 1
+enemy_bullet1 = turtle.Turtle()
+enemy_bullet1.shape("enemy_laser.gif")
+enemy_bullet1.penup()
+enemy_bullet1.speed(0)
+enemy_bullet1.setheading(90)
+enemy_bullet1.hideturtle()
+#this attribute is used to define if bullet has been already shooted
+setattr(enemy_bullet1,"fired",False)
+
+enemy_bullet2 = turtle.Turtle()
+enemy_bullet2.shape("enemy_laser.gif")
+enemy_bullet2.penup()
+enemy_bullet2.speed(0)
+enemy_bullet2.setheading(90)
+enemy_bullet2.hideturtle()
+#this attribute is used to define if bullet has been already shooted
+setattr(enemy_bullet2,"fired",False)
+
+enemy_bullet3 = turtle.Turtle()
+enemy_bullet3.shape("enemy_laser.gif")
+enemy_bullet3.penup()
+enemy_bullet3.speed(0)
+enemy_bullet3.setheading(90)
+enemy_bullet3.hideturtle()
+#this attribute is used to define if bullet has been already shooted
+setattr(enemy_bullet3,"fired",False)
 
 #============= FUNCTION
 #Move the player left and right
@@ -135,6 +174,13 @@ def fire_bullet():
 		bullet.setposition(x,y)
 		bullet.showturtle()
 
+#...
+def fire_bullet_enemy(enemy,bullet):
+	x = enemy.xcor()
+	y = enemy.ycor() - 30
+	bullet.setposition(x,y)
+	bullet.showturtle()
+
 #function to detect collisions between player bullets and enemies
 def isCollision(t1,t2):
 	distance = math.sqrt(math.pow(t1.xcor()-t2.xcor(),2)+math.pow(t1.ycor()-t2.ycor(),2))
@@ -161,6 +207,7 @@ while True:
 		x = enemy.xcor()
 		x += enemyspeed
 		enemy.setx(x)
+		print (getattr(enemy,"fired"))
 
 		#move the enemy back
 		if ( (enemy.xcor() > 270 ) or (enemy.xcor() < -270 )  ) :
@@ -170,21 +217,43 @@ while True:
 				y = e.ycor()	
 				y -= 40
 				e.sety(y)
+		
+		#enemy shoting process...
+		#for each enemie first check if the enemie is killed or not
+		if not ( getattr(enemy, "fired") ): 
+			#only the enemies upper to player can shoot.
+			#this check is acheived checking x coorinate of the player.
+			val = player.xcor() - enemy.xcor()
+			if ( abs(val) < 50):
+			#if (1==1):
+				print ("this enemy is upper to player!")
+				#checking if there are enemy bullets available: only three at same time can be shooted
+				if not ( getattr(enemy_bullet1,"fired") ):
+					fire_bullet_enemy (enemy,enemy_bullet1)
+					setattr(enemy_bullet1,"fired",True) 
+				elif not ( getattr(enemy_bullet2,"fired") ):
+					fire_bullet_enemy (enemy,enemy_bullet2)
+					setattr(enemy_bullet2,"fired",True) 
+				elif not ( getattr(enemy_bullet3,"fired") ):
+					fire_bullet_enemy (enemy,enemy_bullet3)
+					setattr(enemy_bullet3,"fired",True) 
 
 		#Check for a collision between the bullet and the enemy
 		if isCollision(bullet, enemy):
+			setattr(enemy, "fired", True)
 			os.system("afplay enemy_explosion.wav&")
+			enemy.shape("explosion.gif")
 			#reset the bullet
 			bullet.hideturtle()
 			bulletstate = "ready"
 			bullet.setposition(0, -400)
 			#reset the enemy
-			enemy.shape("explosion.gif")
-			time.sleep(0.2)
+			#time.sleep(0.2)
 			#x = random.randint (-200, 200)
 			#y = random.randint (100, 250)
-			#ser alien fired out of screen
-			enemy.setposition(0, 10000)
+			#set alien fired out of screen
+			enemy.setposition(0,10000)
+			enemy.setposition(enemy.xcor(), enemy.ycor())
 			#update the score
 			score += 10
 			scorestring = "Score: %s" %score
@@ -217,7 +286,34 @@ while True:
 		y += bulletspeed
 		bullet.sety(y)
 
+	#move enemy bullets
+	if getattr (enemy_bullet1,"fired"):
+		y = enemy_bullet1.ycor()
+		y -= enemy_bullet_speed
+		enemy_bullet1.sety(y)
+	
+	if getattr (enemy_bullet2,"fired"):
+		y = enemy_bullet2.ycor()
+		y -= enemy_bullet_speed
+		enemy_bullet2.sety(y)
+
+	if getattr (enemy_bullet3,"fired"):
+		y = enemy_bullet3.ycor()
+		y -= enemy_bullet_speed
+		enemy_bullet3.sety(y)
+
 	#Check to see if the bullet has gone to the top
 	if bullet.ycor() > 275:
 		bullet.hideturtle()
 		bulletstate = "ready"
+
+	#Check to see if the enemy bullet has gone to the groud
+	if enemy_bullet1.ycor() == -250:
+		enemy_bullet1.hideturtle()
+		setattr(enemy_bullet1,"fired",False)
+	if enemy_bullet2.ycor() == -250:
+		enemy_bullet2.hideturtle()
+		setattr(enemy_bullet2,"fired",False)
+	if enemy_bullet3.ycor() == -250:
+		enemy_bullet3.hideturtle()
+		setattr(enemy_bullet3,"fired",False)
